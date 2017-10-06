37ec30838939820ff404beaf826e505c064c7086
Manuel Tarazona Querol <s1708793@berrendero.inf.ed.ac.uk>
2017-10-06 12:31:17 +0100

    Demo of PPM file format

---
+++ PPM_Demo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+    ofstream pic;
+    pic.open("pic.ppm");
+    pic << "P3\n200 100 255\n";
+
+    for (int i = 0; i < 100*200*3; i++) {
+        pic << rand() % 256 << " ";
+    }
+    pic.close();
+    return 0;
+}
